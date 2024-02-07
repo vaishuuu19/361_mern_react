5c6442fae2fa9795551afac653a6dff189ccf8d8
vaishuuu19 <158036192+vaishuuu19@users.noreply.github.com>
2024-02-07 20:03:49 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<h1>Hello Word!</h1>
+<p>I am from group-7</p>
+    </div>
+  );
+}
+
+export default App;
